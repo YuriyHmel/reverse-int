56ac1c7d3e81d56e98e840763923606ebe9010fd
YuriyHmel <hmelnich@yandex.ru>
2023-01-18 17:14:35 +0600

    feat: make function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let string = n.toString();
+  let revString = '';
+  let i = (string.length - 1);
+  while (i >= 0) {
+    revString = `${revString}${string[i]}`;
+    i = i - 1;
+  }
+  return(+revString.replace(/[\D]+/g, ''))
 }
